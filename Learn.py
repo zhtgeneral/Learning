c11b6b2e1224592bd5fceec8e4c02c008429fd8d
Zhong <115027357+zhtgeneral@users.noreply.github.com>
2023-06-03 16:36:51 -0700

    Created Learn.py

---
+++ Learn.py
@@ -0,0 +1,21 @@
+import numpy as np
+import tkinter as tk
+
+root = tk.Tk()
+root.title("Python app")
+
+def enter():
+    label.configure(text="Name: " + entry.get())
+
+frame = tk.Frame(root, width=466, height = 333)
+frame.pack()
+
+label = tk.Label(frame, text="Enter Name: ")
+entry = tk.Entry(frame)
+button = tk.Button(frame, text="Enter", command=enter)
+
+label.grid()
+entry.grid()
+button.grid()
+
+root.mainloop()
